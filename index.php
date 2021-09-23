cfe2c0a6482f3400b2287c4871fc0090e6be2850
Jonathan Bossenger <jonathanbossenger@gmail.com>
2021-09-23 15:03:39 +0200

    PR refactor

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ putenv("GOOGLE_APPLICATION_CREDENTIALS=credentials.json");
 /* Require composer modules */
 require __DIR__ . '/vendor/autoload.php';
 
+use Google\Cloud\Kms\V1\KeyManagementServiceClient as Kms;
+use Google\Cloud\Kms\V1\CryptoKey;
+use Google\Cloud\Kms\V1\KeyRing;
 use MyCloudKmsProject\KeyManager;
 
 $projectId   = 'project-id-123456';
@@ -13,6 +16,9 @@ $keyRingId   = 'keyring';
 $cryptoKeyId = 'key';
 
 $keyManager = new KeyManager(
+    new Kms(),
+    new KeyRing(),
+    new CryptoKey(),
     $projectId,
     $location,
     $keyRingId,
